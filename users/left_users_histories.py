6fe330a1edd34b440b67e3d8d23364ba185f7b9b
M Bernt <m.bernt@ufz.de>
2025-09-03 10:58:07 +0200

    left_users_histories: ignore purged histories (#17)
    * left_users_histories: ignore archived and purged histories
    
    * also backup archived histories

--- users/left_users_histories.py
+++ users/left_users_histories.py
@@ -102,13 +102,16 @@ history_client = HistoryClient(galaxy_instance)
 offset = 1
 while True:
     histories = history_client.get_histories(
-        all=True, limit=USER_BATCH_SIZE, offset=offset, keys=["id", "user_id", "size"]
+        all=True, limit=USER_BATCH_SIZE, offset=offset, keys=["id", "user_id", "size", "purged"]
     )
     if not histories:
         break
     offset += USER_BATCH_SIZE
 
     for history in histories:
+        if history["purged"]:
+            logger.info(f"Ignoring history {history['id']} purged {history['purged']}")
+            continue
         user_id = history["user_id"]
         size = history["size"]
         if user_id not in user_by_id:
